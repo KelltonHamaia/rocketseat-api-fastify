1f2f3b8ed472b4096c835929802465d024a27a2f
kellton_hamaia <hamaia.kell@gmail.com>
2025-08-30 22:17:47 -0300

    adicionar dockefile

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { server } from './app.ts'
 
-await server.listen({ port: 3333 }).then(() => {
+await server.listen({ port: 3333, host: '0.0.0.0 ' }).then(() => {
     console.log('Running!')
 })
